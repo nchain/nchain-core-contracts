1b922d1a40bd9db941a8f154ca5ef13996c19ff3
chenxinhua2018 <15220088021@163.com>
2020-12-05 17:08:33 +0800

    improve the check for order

--- dex/src/dex.cpp
+++ dex/src/dex.cpp
@@ -4,13 +4,35 @@
 using namespace eosio;
 using namespace std;
 
+namespace order_type {
+   static const string_view LIMIT_PRICE = "limit_price";
+   static const string_view MARKET_PRICE = "market_price";
+   static const set<string_view> MODES = {
+      LIMIT_PRICE, MARKET_PRICE
+   };
+   inline bool is_valid(const string_view &mode) {
+      return MODES.count(mode);
+   }
+}
+
+namespace order_side {
+   static const string_view BUY = "buy";
+   static const string_view SELL = "sell";
+   static const set<string_view> MODES = {
+      BUY, SELL
+   };
+   inline bool is_valid(const string_view &mode) {
+      return MODES.count(mode);
+   }
+}
+
 namespace open_mode {
-   static const string PUBLIC = "public";
-   static const string PRIVATE = "private";
-   static const set<string> MODES = {
+   static const string_view PUBLIC = "public";
+   static const string_view PRIVATE = "private";
+   static const set<string_view> MODES = {
       PUBLIC, PRIVATE
    };
-   inline bool valid(const string &mode) {
+   inline bool is_valid(const string_view &mode) {
       return MODES.count(mode);
    }
 }
@@ -29,8 +51,18 @@ uint64_t parse_ratio(string_view str) {
    return ret.value;
 }
 
+string parse_order_type(string_view str) {
+    check(order_type::is_valid(str), "invalid order_type=" + string{str});
+    return string{str};
+}
+
+string parse_order_side(string_view str) {
+    check(order_side::is_valid(str), "invalid order_side=" + string{str});
+    return string{str};
+}
+
 string parse_open_mode(string_view str) {
-    // TODO: check open mode valid
+    check(open_mode::is_valid(str), "invalid open_mode=" + string{str});
     return string{str};
 }
 
@@ -51,15 +83,33 @@ void dex::ontransfer(name from, name to, asset quantity, string memo) {
     if (params.size() == 7 && params[0] == "order") {
       // order:<type>:<side>:<coin_quant>:<asset_quant>:<price>:<ex_id>
         order_t order;
-        order.order_type = string{params[1]};
-        order.order_side = string{params[2]};
+        order.order_type = parse_order_type(params[1]);
+        order.order_side = parse_order_side(params[2]);
         order.coin_quant = asset_from_string(params[3]);
         order.asset_quant = asset_from_string(params[4]);
         order.price = parse_price(params[5]);
+
+        // TODO: check coin_pair exist
+
+        // check amount
+        if (order.order_type == order_type::MARKET_PRICE && order.order_side == order_side::BUY) {
+            check(order.coin_quant.amount == quantity.amount, "coin amount must be equal to the transfer quantity");
+            // TODO: check coin amount range
+            check(order.asset_quant.amount == 0, "asset amount must be 0 for market buy order");
+        } else {
+            check(order.coin_quant.amount == 0, "coin amount must be 0");
+            check(order.asset_quant.amount == quantity.amount, "asset amount must be equal to the transfer quantity");
+            // TODO: check asset amount range
+        }
+
+        // TODO: need to add the total order coin/asset amount?
+
         order_table order_tbl(get_self(), get_self().value);
         order.order_id = order_tbl.available_primary_key();
         order.owner = from;
+
         check(order_tbl.find(order.order_id) == order_tbl.end(), "The order is exist. order_id=" + std::to_string(order.order_id));
+
         order_tbl.emplace( from, [&]( auto& o ) {
             o = order;
         });
