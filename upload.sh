df7f552a2cf920e2c32f655d32247bc79eb3ea96
tigerich <tigerich@163.com>
2021-02-28 16:09:32 +0800

    update upload

--- upload.sh
+++ upload.sh
@@ -1,13 +1,21 @@
-# host=jw
-host=sh-test
+#!/bin/bash
 
-rsync -rav -e ssh \
-    --include='*.abi' \
-    --include='*.wasm' \
-    --exclude='**/CMakeFiles' \
-    --exclude='*.cmake' \
-    --exclude='Makefile' \
-    --exclude='*.md' \
-    --exclude='CMakeCache.txt' \
-    --exclude='CMakeLists.txt' \
-    ./build/contracts/ ${host}:/opt/mgp/wallet/contracts
+sync() {
+    host=$1
+
+    rsync -rav -e ssh \
+        --include='*.abi' \
+        --include='*.wasm' \
+        --exclude='**/CMakeFiles' \
+        --exclude='*.cmake' \
+        --exclude='Makefile' \
+        --exclude='*.md' \
+        --exclude='CMakeCache.txt' \
+        --exclude='CMakeLists.txt' \
+        ./build/contracts/ ${host}:/opt/mgp/wallet/contracts
+
+    rsync -rav -e ssh ./unittest.sh ${host}:/opt/mgp/wallet/
+}
+
+sync jw
+sync sh-test
